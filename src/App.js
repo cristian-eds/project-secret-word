bf247d02b5d07aa9390d5f1d280c7605cf17fd4d
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-01-04 20:48:01 -0300

    Changing game stages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import StartScreen from './components/StartScreen';
 import Game from './components/Game';
 import GameOver from './components/GameOver';
 
-const stage = [
+const stages = [
   {id:1, name: "start"},
   {id:2, name: "game"},
   {id:3, name: "end"}
@@ -20,14 +20,29 @@ const stage = [
 
 function App() {
 
-  const [gameStage, setGameStage] = useState(stage[0].name);
+  const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+
+  // start game secret words
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // restart game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame}/>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
